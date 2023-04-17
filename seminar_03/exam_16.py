07f180986193396c9933018bb92d4df6748c5141
Zhanaydarov Asset <asset0889@mail.ru>
2023-04-17 21:21:59 +0500

    16 zadanie resheno

---
+++ seminar_03/exam_16.py
@@ -0,0 +1,15 @@
+
+import random
+
+size = int(input('Введите размер списка: '))
+array = []
+find_numb = int(input(f'поиск числа (от 1 до 10)'))
+count = 0
+for i in range (size-1):
+    array.append(random.randint(1, 10))
+for i in array:
+    if find_numb == i:
+        count += 1
+
+print(count)
+print(array)
